87d179b6a904ababbeb4b2292f5f32dcf8d240d3
dolto <hdw810@daum.net>
2024-01-15 23:33:48 +0900

    원본의 변경파일까지 병합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
-import Banner from "./component/banner"
+import Banner from "@/app/component/banner";
+import BannerList from "@/app/component/BannerList";
+import React from "react";
+
 
 export default function Home() {
+    let a = [1,2,3];
     return (
         <main>
-            <Banner/>
+            <section className="banner">
+                <Banner>
+                    {
+                        a.map((value, index, array) => {
+                            return (
+                                <BannerList key={index} props={value}></BannerList>
+                            )
+                        })
+                    }
+                </Banner>
+            </section>
+
         </main>
     )
 }
