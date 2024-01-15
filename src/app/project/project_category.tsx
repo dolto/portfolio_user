fb812f5a8998cc99d13b7d72efc1ac7c2fcd7ae2
dolto <hdw810@daum.net>
2024-01-15 23:28:19 +0900

    Project List 임시 기능 제작

--- src/app/project/project_category.tsx
+++ src/app/project/project_category.tsx
@@ -41,7 +41,7 @@ export default async function Project_category(props: Prop){
                       langs_slecets.length == langsDB.length ?
                           [lang[0]] : langs_slecets.includes(lang[0]) ?
                               langs_slecets.filter(s => !(lang[0] == s)) : langs_slecets.concat(lang[0])
-                  )}&skills_slects=${JSON.stringify(skills_slects)}`}>
+                  )}&skills_slects=${JSON.stringify(skills_slects)}`} key={lang[0]}>
                       {langs_slecets.includes(lang[0])?
                           <img className={'project_category_icon project_icon_selected'} src={lang[1]}/>:
                           <img className={'project_category_icon'} src={lang[1]}/>}
@@ -50,13 +50,13 @@ export default async function Project_category(props: Prop){
           </nav>
           <i className="fas fa-duotone fa-circle-arrow-right fa-3x"></i>
           <i className="fas fa-duotone fa-circle-arrow-left fa-3x"></i>
-          <nav className={'project_langs'}>
+          <nav className={'project_skills'}>
               {skillsDB.map(skill =>
                   <Link href={`project?langs_slecets=${JSON.stringify(langs_slecets)}&skills_slects=${JSON.stringify(
                       skills_slects.length == skillsDB.length ?
                           [skill[0]] : skills_slects.includes(skill[0]) ? 
                               skills_slects.filter(s => !(skill[0] == s)) : skills_slects.concat(skill[0])
-                  )}`}>
+                  )}`} key={skill[0]}>
                       {skills_slects.includes(skill[0])?
                           <img className={'project_category_icon project_icon_selected'} src={skill[1]}/>:
                           <img className={'project_category_icon'} src={skill[1]}/>}
