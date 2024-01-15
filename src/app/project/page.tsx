a85539e30110adf539c67a67c683ef5fbe2f1068
dolto <hdw810@daum.net>
2024-01-15 19:47:15 +0900

    project page local commit

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,7 +1,31 @@
-function Project() {
+import Project_category from "./project_category";
+
+const langs = [
+    'Java',
+    'Kotlin',
+    'Rust',
+]
+const skills = [
+	'Spring',
+    'Spring Boot',
+    'Bevy',
+]
+interface Prop{
+	searchParams: {
+		langs_slecets: string,
+		skills_slects: string
+	}
+}
+function Project(props: Prop) {
+	console.log(props)
 	return (
 		<section>
-
+			<Project_category searchParams={{
+				langs_slecets: props.searchParams.langs_slecets == undefined ?
+					[]:JSON.parse(props.searchParams.langs_slecets),
+				skills_slects: props.searchParams.skills_slects == undefined ?
+					[]:JSON.parse(props.searchParams.skills_slects)
+			}}></Project_category>
 		</section>
 	)
 }
--- src/app/project/project_category.tsx
+++ src/app/project/project_category.tsx
@@ -1,8 +1,68 @@
-import "./project.css"
+import Link from "next/link";
+import "./project.css";
 
-export default  function Project_category(props: any){
+interface Prop{
+    searchParams: {
+        langs_slecets: string[],
+        skills_slects: string[]
+    }
+}
+
+export default async function Project_category(props: Prop){
+    const langsDB: [string, string][] = [
+        ['Java', ''],
+        ['Kotlin', ''],
+        ['Rust', ''],
+    ]; //MongoDB에서 DB가져올 것 일단은 임시
+    const skillsDB: [string, string][] = [
+        ['Spring', ''],
+        ['Spring Boot', ''],
+        ['Bevy', ''],
+    ]; //MongoDB에서 DB가져올 것 일단은 임시
+    const langs_slecets : string[] = props.searchParams.langs_slecets;
+    const skills_slects : string[] = props.searchParams.skills_slects;
+    const langs: [string,string][] = langs_slecets.length == 0 ? langsDB :
+        langsDB.filter(
+        db => langs_slecets.includes(db[0])
+    );
+    const skills: [string,string][] = skills_slects.length == 0 ? skillsDB :
+        skillsDB.filter(
+        db => skills_slects.includes(db[0])
+    );
+    console.log(props)
     return(
-      <>
-      </>
+      <article className={"project_category_selecter"}>
+          <i className="fas fa-duotone fa-circle-arrow-left fa-3x"></i>
+          <nav className={'project_langs'}>
+              {langsDB.map(lang =>{
+                  console.log(langs_slecets.includes(lang[0]));
+                  console.log(lang[0]);
+                  return( <Link href={`project?langs_slecets=${JSON.stringify(
+                      langs_slecets.length == langsDB.length ?
+                          [lang[0]] : langs_slecets.includes(lang[0]) ?
+                              langs_slecets.filter(s => !(lang[0] == s)) : langs_slecets.concat(lang[0])
+                  )}&skills_slects=${JSON.stringify(skills_slects)}`}>
+                      {langs_slecets.includes(lang[0])?
+                          <img className={'project_category_icon project_icon_selected'} src={lang[1]}/>:
+                          <img className={'project_category_icon'} src={lang[1]}/>}
+                  </Link>)})
+              }
+          </nav>
+          <i className="fas fa-duotone fa-circle-arrow-right fa-3x"></i>
+          <i className="fas fa-duotone fa-circle-arrow-left fa-3x"></i>
+          <nav className={'project_langs'}>
+              {skillsDB.map(skill =>
+                  <Link href={`project?langs_slecets=${JSON.stringify(langs_slecets)}&skills_slects=${JSON.stringify(
+                      skills_slects.length == skillsDB.length ?
+                          [skill[0]] : skills_slects.includes(skill[0]) ? 
+                              skills_slects.filter(s => !(skill[0] == s)) : skills_slects.concat(skill[0])
+                  )}`}>
+                      {skills_slects.includes(skill[0])?
+                          <img className={'project_category_icon project_icon_selected'} src={skill[1]}/>:
+                          <img className={'project_category_icon'} src={skill[1]}/>}
+                  </Link>)}
+          </nav>
+          <i className="fas fa-duotone fa-circle-arrow-right fa-3x"></i>
+      </article>
     );
 }
