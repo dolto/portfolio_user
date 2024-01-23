1601baa64b4248a84e91862aa1a7d40a9adbb857
dolto <hdw810@daum.net>
2024-01-23 15:56:36 +0900

    project 페이지 DB연동 1차
    ## 특이사항
    - interface.ts을 갱신

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,67 +1,7 @@
-import {ContentMode, PostCollection } from "./interface";
+import { connectDB } from "../../../Util/MongoDB";
+import { PostCollection } from "./interface";
 import Project_category from "./project_category";
 import Project_list from "./project_list";
-
-const langs = [
-    'Java',
-    'Kotlin',
-    'Rust',
-]
-const skills = [
-	'Spring',
-    'Spring Boot',
-    'Bevy',
-]
-const project_data: PostCollection[] = [
-	{
-		id:'0',
-		category: ['JavaId','KotlinId'],
-		skill_id: ['SpringId'],
-		contents: [
-			{
-				mode:ContentMode.img,
-				content:'https://raw.githubusercontent.com/dolto/IBA_Cocktail_recipe_API/main/Cocktail/Alejandro.webp'
-			}
-		],
-		name: '스프링으로 시작한 포트폴리오 사이트'
-	},
-	{
-		id:'1',
-		category: ['RustId'],
-		skill_id: ['BevyId'],
-		contents: [
-			{
-				mode:ContentMode.img,
-				content:'https://raw.githubusercontent.com/dolto/IBA_Cocktail_recipe_API/main/Cocktail/Alejandro.webp'
-			}
-		],
-		name: '러스트로 시작하는 게임개발'
-	},
-	{
-		id:'2',
-		category: ['JavaId','KotlinId'],
-		skill_id: ['Spring BootId'],
-		contents: [
-			{
-				mode:ContentMode.img,
-				content:'https://raw.githubusercontent.com/dolto/IBA_Cocktail_recipe_API/main/Cocktail/Alejandro.webp'
-			}
-		],
-		name: '스프링부트로 시작한 포트폴리오 사이트'
-	},
-	{
-		id:'3',
-		category: ['KotlinId'],
-		skill_id: ['SpringId'],
-		contents: [
-			{
-				mode:ContentMode.img,
-				content:'https://raw.githubusercontent.com/dolto/IBA_Cocktail_recipe_API/main/Cocktail/Alejandro.webp'
-			}
-		],
-		name: '스프링부트로 시작한 코틀린 포트폴리오 사이트'
-	}
-]
 
 interface Prop{
 	searchParams: {
@@ -69,9 +9,11 @@ interface Prop{
 		skills_slects: string
 	}
 }
-function Project(props: Prop) {
-	console.log(process.env.MongoDB_URI)
-
+async function Project(props: Prop) {
+	const client = await connectDB;
+	const db = client.db('folio');
+	const project_data = await db.collection<PostCollection>('PostData').find().toArray();
+	console.log(project_data);
 	return (
 		<section>
 			<Project_category searchParams={{
