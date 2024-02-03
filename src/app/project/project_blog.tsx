c26bf62f5e9072b5c83af2242ebee1ad9d08d0b0
dolto <hdw810@naver.com>
2024-02-03 18:17:10 +0900

    간단한 커밋

--- src/app/project/project_blog.tsx
+++ src/app/project/project_blog.tsx
@@ -6,7 +6,7 @@ interface Props{
     is_Blog: string|undefined
 }
 export default function ProjectBlog(prop: Props){
-    console.log(location.search);
+    //console.log(location.search);
     const is_blog_string = RegExp("is_blog=(true|false)");
     let search_blog = prop.is_Blog === undefined?"?is_blog=true": location.search.replace(is_blog_string,"is_blog=true");
 	let search_project = prop.is_Blog === undefined?"?is_blog=false": location.search.replace(is_blog_string,"is_blog=false");
