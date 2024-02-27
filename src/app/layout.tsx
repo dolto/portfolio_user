c058beaf3f3dd47029bc960cbf568f6384f3fff3
Leia1ra <leiaira92@gmail.com>
2024-02-28 00:54:05 +0900

    모바일 배포 환경 태스트용 코드 삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Link from "next/link";
-import Image from "next/image";
 import React from "react";
-import Head from "next/head";
 export const metadata: Metadata = {
     title: 'portfolio',
     description: '',
@@ -12,20 +10,17 @@ export const metadata: Metadata = {
 function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        </Head>
         <body>
         <header className="main-header">
             <h1><Link href="/">주인장 정보</Link></h1>
-                    <div className="main-header-section-line"></div>
-                    <nav>
-                        <Link href={"/resume"}>Resume</Link>
-                        <Link href={"/guest"}>GuestBook</Link>
-                    </nav>
-                </header>
-                {children}
-            </body>
+            <div className="main-header-section-line"></div>
+            <nav>
+                <Link href={"/resume"}>Resume</Link>
+                <Link href={"/guest"}>GuestBook</Link>
+            </nav>
+        </header>
+        {children}
+        </body>
         </html>
     )
 }
