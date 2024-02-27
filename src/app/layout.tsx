39224320f13a9443b69b24f97f9609a60817964e
Leia1ra <leiaira92@gmail.com>
2024-02-28 00:39:28 +0900

    모바일 배포 환경 태스트용 코드 삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,21 @@ import './globals.css'
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import Head from "next/head";
 export const metadata: Metadata = {
     title: 'portfolio',
     description: '',
+
 }
 function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>
-                <header className="main-header">
-                    <h1><Link href="/">주인장 정보</Link></h1>
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </Head>
+        <body>
+        <header className="main-header">
+            <h1><Link href="/">주인장 정보</Link></h1>
                     <div className="main-header-section-line"></div>
                     <nav>
                         <Link href={"/resume"}>Resume</Link>
